954077503de0019a8b46c629110b9c19d5afa2f2
guldenmw <guldenmw@gmail.com>
2020-04-01 09:14:40 +0200

    - setup eslint and styled components - started making layout and navbar

---
+++ src/components/nav-bar/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+
+const StyledNavBar = styled.div`
+  flex-grow: 1;
+  
+  .menu-button {
+    margin-right: 10px;
+  }
+  
+  .title {
+  flex-grow: 1;
+  }
+`;
+
+export default StyledNavBar;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as firebase from 'firebase/app';
 import App from './core/App';
-import * as serviceWorker from './serviceWorker';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDQI8BBnXotgciNZ-tvzKfUKAGVG19mLFk',
+  authDomain: 'wishlist-cc7c7.firebaseapp.com',
+  databaseURL: 'https://wishlist-cc7c7.firebaseio.com',
+  projectId: 'wishlist-cc7c7',
+  storageBucket: 'wishlist-cc7c7.appspot.com',
+  messagingSenderId: '632433740720',
+  appId: '1:632433740720:web:15fbb41be9938ff6b36475',
+  measurementId: 'G-T803HB078L',
+};
+
+firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
